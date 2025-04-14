bc7e860f4c56a074435f7b3705941d2120e32f70
Phvr06 <ribeirophvr@gmail.com>
2025-04-14 10:51:57 -0300

    ex1

--- exe1/main.c
+++ exe1/main.c
@@ -30,6 +30,8 @@ void i2c_task(void *p) {
 
     // TODO
     // Configure o acc para operar em 4G
+    uint8_t buf_write2[2] = {0x1C, 1 << 3};
+    i2c_write_blocking(i2c_default, I2C_CHIP_ADDRESS, buf_write2, 2, false);
 
     while (1) {
         vTaskDelay(pdMS_TO_TICKS(200));
